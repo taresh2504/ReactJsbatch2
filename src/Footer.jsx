0a019e609ef8e17e726fc092ebf8d4e48b8e8a8e
taresh2504 <taresh25202@gmail.com>
2025-12-27 13:34:36 +0530

    app.jsx,navbar,js

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 let Footer =()=>{
     return(
         <>
-        <h2 style={{color:'red', fontSize:'50px' , backgroundColor:'yellow'}}>This is Footer</h2>
+        <h2 style={{color:'red'}}>This is Footer</h2>
         </>
     )
 }
