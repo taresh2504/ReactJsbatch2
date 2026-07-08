5fde3be127b069f97e8f8514959d8e83d1673506
taresh2504 <taresh25202@gmail.com>
2026-07-08 22:48:01 +0530

    update

--- src/Adminpanel.jsx
+++ src/Adminpanel.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
-
+// AdminPanel.jsx 
 const AdminPanel = () => {
 
   const [title, setTitle] = useState('')
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import AppRoutes from "./Routes";
 import AdminPanel from "./Adminpanel";
 import ChatApp from "./ChatApp";
 
-
+// App.jsx
 let App = () => {
   return (
   // <AppRoutes />
