ccc9ee335e0e8cdadebdc99c3757a07bc1ff332e
taresh2504 <taresh25202@gmail.com>
2026-08-15 21:06:24 +0530

    update

---
+++ src/Topics.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Topics = () => {
+  return (
+    <>
+      <div>
+        react topics :- 
+1.intro
+2.folder create and installing react 
+3. understanding folder structure
+4. src,App.jsx,navbar.jsx,Javascript likhna 
+5. inline css in react 
+6. css,named export 
+7. adding images through public,src,Props,child,destructuring
+8. Props and child destructuring with card making task both static and dynamic
+9. Tailwind css installation and setup and working
+10.empty tag or react fragment,Navigation bar,map in react 
+11.react router dom, routes,route,layout.jsx,link,outlet and moving from one page to another
+12. button, show error page not found,use navigate, usestate in Hook.jsx 
+13. changing bg color,button text,show hide using ternary operator 
+14. Form handling, onchange event 
+15. Form validation
+16. project assigned railway website 
+17. local storage 
+18. login and signup 
+19. react icons and pending project work
+20. db.json file,useeffect 
+21. get and delete from api using axios
+22.  Booking form 
+23. booking form k data ko db.json me bhejna or bookings m jaake dekhna 
+24. edit method 
+25. search feature added 
+26. context api 
+      </div>
+    </>
+  )
+}
+
+export default Topics
